d3d6942c66d6c610ebefc58ccaa341b46bef2f35
Phoebe-mzc <96410002+Phoebe-mzc@users.noreply.github.com>
2021-12-24 18:12:41 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Plum Commerce</title>
+    <link href="style.css" media="all" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body>
+    <div id="wrapper">
+
+      <?php include('menu.php'); ?>
+
+      <?php include("get-index-meta-data.php"); ?>
+
+      <hr />
+
+      <?php include("get-root-dir.php"); ?>
+
+    </div>
+  </body>
+</html>
