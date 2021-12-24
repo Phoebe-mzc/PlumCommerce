aca5b9236455043b4d457caf9c4f980605be8b49
Phoebe-mzc <96410002+Phoebe-mzc@users.noreply.github.com>
2021-12-24 18:14:59 +0900

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,7 @@
+<div id="header"><a href="/"><img src="https://workshop.mzcloud.xyz/AWS-101-2.2.9/download/MEGAZONECLOUD_LOGO.png" width="600" height="154" /></a></div>
+
+<div id='menu'>
+<ul>
+  <li><a href="load.php">Load Test</a></li>
+</ul>
+</div>
